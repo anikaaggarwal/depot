bee655314ed91202ffb94b687a94f2b632094906
anikaaggarwal <anika.aggarwal1@gmail.com>
2018-05-30 20:33:41 +0530

    Connecting products to carts

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -19,7 +19,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Product.count') do
       post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
     end
-
     assert_redirected_to product_url(Product.last)
   end
 
@@ -38,10 +37,19 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
-    end
+  end
 
     assert_redirected_to products_url
   end
